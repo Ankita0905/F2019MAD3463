56f92eb9dd36bb7b2d393fff0bf8d5652a37bbd2
macstudent <ankita.jain12342@gmail.com>
2019-09-25 18:19:30 -0400

    for loop

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -6,7 +6,13 @@ public  static String reverse(String s)
 {
     String revs = new String();
     String[] words = s.split(" ");
-   
+    for(int i = 0; i < words.length; i++)
+    {
+
+        String rev = new StringBuffer(words[i]).reverse().toString();
+        revs = revs + rev;
+    }
+  
 
     return s;
 
